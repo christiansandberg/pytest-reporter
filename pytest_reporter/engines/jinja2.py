fb1f43045bc938a1e90049b41e8ff8082fe35800
Christian Sandberg <christiansandberg@me.com>
2020-04-20 22:19:04 +0200

    Make Jinja optional

--- pytest_reporter/engines/jinja2.py
+++ pytest_reporter/engines/jinja2.py
@@ -1,17 +1,21 @@
-import pytest_reporter
 import pytest
-from jinja2 import (
-    Environment,
-    ChoiceLoader,
-    FileSystemLoader,
-    PackageLoader,
-    select_autoescape,
-)
+try:
+    from jinja2 import (
+        Environment,
+        FileSystemLoader,
+        select_autoescape,
+    )
+except ImportError:
+    HAS_JINJA2 = False
+else:
+    HAS_JINJA2 = True
 
 
 @pytest.hookimpl(trylast=True)
 def pytest_reporter_make_env(template_dirs, config):
     if config.getoption("--template-engine") == "jinja2":
+        if not HAS_JINJA2:
+            raise RuntimeError("Jinja2 template engine not installed")
         return Environment(
             loader=FileSystemLoader(template_dirs),
             autoescape=select_autoescape(["html", "htm", "xml"]),
@@ -22,5 +26,5 @@ def pytest_reporter_make_env(template_dirs, config):
 
 @pytest.hookimpl(trylast=True)
 def pytest_reporter_render(env, template, context):
-    if isinstance(env, Environment):
+    if HAS_JINJA2 and isinstance(env, Environment):
         return env.get_template(template).render(**context)
